fd74e782f14977f6f726851756fc7b37e4e5f915
黄溪舟 <xizhouh@gmail.com>
2017-10-30 01:17:28 -0600

    Add Yahoo Weather Api

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const program = require('commander')
 const chalk = require('chalk')
 const axios = require('axios')
+const YQL = require('yql')
+const inquirer = require('inquirer')
+const _ = require('lodash')
 
 const URL_IP = 'http://api.map.baidu.com/location/ip'
 const URL_FORECAST = 'http://api.map.baidu.com/telematics/v3/weather'
@@ -28,7 +31,52 @@ program
 				console.log(chalk.red('Whoops!!! There is something wrong with your network.'))
 			} else {
 				if(res.data.status !== 0) {
-					console.log(chalk.red('Oye!!! You are in abroad.'))
+					console.log(chalk.red('Oye!!! You are not in China.'))
+					console.log('')
+					console.log('so...')					
+					let config = _.assign({
+							cityName: ''
+					}, option)
+					let promps = []
+					if(config.cityName !== 'string') {
+						promps.push({
+							type: 'input',
+							name: 'cityName',
+							message: 'Input your cityname:',
+							validate: input => {
+								if(!input) {
+									return chalk.red('You should input something here...')
+								}
+								return true
+							}
+						})
+					}
+					inquirer.prompt(promps).then(answers => {
+						var query = new YQL(`select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="${answers.cityName}")`)
+						query.exec((err, data) => {
+							if(err) {
+								console.log(chalk.red('emmmmm... I don\'t know what happened.'))
+							} else {
+								if(!data.query.count) {
+									console.log(chalk.red('Please... Enter the correct city name.'))								
+								} else {
+									let resource = data.query.results.channel.item
+									console.log(
+										`City: ${chalk.green(answers.cityName)} ❤️\n` +
+										`Date: ${chalk.green(resource.condition.date)}\n` +
+										`Temp: ${chalk.green(resource.condition.temp)}${chalk.green('F')} | ${chalk.green(resource.condition.text)}`										
+									)
+									resource.forecast.forEach(item => {
+										console.log(
+											`${item.day}: ${chalk.green(item.low)}${chalk.green('F')} - ${chalk.green(item.high)}${chalk.green('F')} | ${chalk.green(item.text)}`
+										)
+									}, this)
+									console.log('')
+									console.log(chalk.yellow('Have a nice day~'))
+								}
+							}
+						})
+        	}) 
 				} else {
 					getweatherModel.location = res.data.content.address_detail.city
 					axios.get(URL_FORECAST, {
