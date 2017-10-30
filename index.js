d3cb9dd90a8dd1da77f47aa0e6806547abab90be
黄溪舟 <xizhouh@gmail.com>
2017-10-29 20:07:08 -0600

    fix bugs

--- index.js
+++ index.js
@@ -26,37 +26,41 @@ program
 			if(res.status !== 200) {
 				console.log(chalk.red('Whoops!!! There is something wrong with your network.'))
 			} else {
-				getweatherModel.location = res.data.content.address_detail.city
-				axios.get('http://api.map.baidu.com/telematics/v3/weather', {
-					params: getweatherModel
-				})
-				.then(res => {
-					if(res.status !== 200) {
-						console.log(chalk.red('Whoops!!! There is something wrong with your network.'))
-					} else {
-						let resource = res.data
-						if(resource.status !== 'success') {
-							console.log(chalk.red('emmmmm... I don\'t know what happened.'))
+				if(res.data.status !== 0) {
+					console.log(chalk.red('Oye!!! You are in abroad.'))
+				} else {
+					getweatherModel.location = res.data.content.address_detail.city
+					axios.get('http://api.map.baidu.com/telematics/v3/weather', {
+						params: getweatherModel
+					})
+					.then(res => {
+						if(res.status !== 200) {
+							console.log(chalk.red('Whoops!!! There is something wrong with your network.'))
 						} else {
-							console.log(
-								`城市: ${chalk.green(resource.results[0].currentCity)} ❤️\n` +
-								`日期: ${chalk.green(resource.date)}\n` +
-								`pm2.5: ${chalk.green(resource.results[0].pm25)}`
-							)
-							resource.results[0].weather_data.forEach(item => {
+							let resource = res.data
+							if(resource.status !== 'success') {
+								console.log(chalk.red('emmmmm... I don\'t know what happened.'))
+							} else {
 								console.log(
-									`${item.date.slice(0, 2)}: ${chalk.green(item.weather)} | ${chalk.green(item.temperature)}`
+									`城市: ${chalk.green(resource.results[0].currentCity)} ❤️\n` +
+									`日期: ${chalk.green(resource.date)}\n` +
+									`pm2.5: ${chalk.green(resource.results[0].pm25)}`
 								)
-							}, this)
-							console.log('')
-							console.log(chalk.yellow('Have a nice day~'))
+								resource.results[0].weather_data.forEach(item => {
+									console.log(
+										`${item.date.slice(0, 2)}: ${chalk.green(item.weather)} | ${chalk.green(item.temperature)}`
+									)
+								}, this)
+								console.log('')
+								console.log(chalk.yellow('Have a nice day~'))
+							}
 						}
-					}
-				})
+					})
+				}
 			}
 		})
 		.catch(error => {
-			console.log('emmmmm... I don\'t know what happened.')
+			console.log(chalk.red('emmmmm... I don\'t know what happened.'))
 		})
 	})
 	.on('--help', () => {
